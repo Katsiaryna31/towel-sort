5bdbe176440f0a08d1c1e1e58300b2a8e9d3a2b5
Katsiaryna31 <katia310790@mail.ru>
2021-07-20 22:27:17 +0300

    add ready task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return [];
+  }
+  let arr = [];
+  matrix.map(array => {
+    if (matrix.indexOf(array)%2 === 0) {
+      array.forEach(el=> {
+        arr.push(el);
+      })
+    } else {
+      for (let i = array.length-1; i>=0; i--) {
+        arr.push(array[i])
+      };
+    }
+  })
+  return arr;
 }
